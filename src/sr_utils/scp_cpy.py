763e7aa24ff7eb1eddc8b29b4a8de198064111d5
DaniAffCH <danieleaffinita2000@gmail.com>
2023-06-14 13:51:58 +0000

    old stuff that i do not remember

--- src/sr_utils/scp_cpy.py
+++ src/sr_utils/scp_cpy.py
@@ -2,10 +2,10 @@ import subprocess
 import os
 import time
 key = "spqr"
-path = "~/Desktop"
+path = "~/Desktop/daniele_stuff"
 ip = "192.168.0.42"
 remote_user = "spqrnao"
-file = "*.png"
+file = "../images_inf/*.png"
 
 update = 10
 
@@ -14,5 +14,6 @@ while True:
 	subprocess.run(bash_command, shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
 	bash_command = f"rm {file}"
 	subprocess.run(bash_command, shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+	print(f"[COPYFILE] PID = {os.getpid()}")
 	time.sleep(update)
 
