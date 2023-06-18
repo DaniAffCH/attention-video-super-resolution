5958613d3e07d02c585bfc2ab9e4a63e2ee5107b
DaniAffCH <danieleaffinita2000@gmail.com>
2023-06-18 17:50:21 +0200

    inference stuff
    Co-authored-by: francomano <playerhorus@gmail.com>

--- src/inference/sr_inference.py
+++ src/inference/sr_inference.py
@@ -11,15 +11,19 @@ import numpy
 import torchvision.transforms.functional
 import tqdm
 
+@torch.no_grad()
 def inference(conf, testing = False):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(f"Using {device}")
-    num_vid = conf["INFERENCE"].getint("video_number")
+    #num_vid = conf["INFERENCE"].getint("video_number")
     path = conf["INFERENCE"]["save_path"]
-    model = Generator(conf).to(device)
+    model = Generator(conf)
     model.load_state_dict(torch.load("trained_models/"+conf['TRAINING'].get("name_model")))
+    size=(conf["DEFAULT"].getint("image_height"),conf["DEFAULT"].getint("image_width"))
 
     data_loader = getDataLoader(conf, "train")
+
+    model.eval()
 
     if not os.path.exists(path):
         os.makedirs(path)
@@ -28,26 +32,48 @@ def inference(conf, testing = False):
     for sample in tqdm.tqdm(data_loader):
         s=torch.stack(sample["x"],dim=0)
         s=s.permute(1,0,4,2,3).to(device)
-        
+
+        ups= torch.nn.Upsample(size=(720, 1280), mode='bilinear', align_corners=None, recompute_scale_factor=None)
+        upsampled = s[:,len(sample["x"])//2,:,:,:].cpu()
+        upsampled = ups(upsampled)
+        model.to(device)
         y=model(s)
-        y=y.cpu()
-        residual = torch.abs(y[0].permute(1,2,0).detach() - sample["x"][len(sample["x"])//2][0])
+        model = model.to("cpu")
+        y=torch.nn.functional.interpolate(y, size=size, mode='bilinear', align_corners=None, recompute_scale_factor=None)
+  
+        for i in range(conf['INFERENCE'].getint("n_updates")):
+            torch.cuda.empty_cache()
+            s[:,model.center_frame_index,:,:,:]=y
+            del y
+            model = model.to(device)
+            y=model(s)
+            model = model.to("cpu")
+            y=torch.nn.functional.interpolate(y, size=size, mode='bilinear', align_corners=None, recompute_scale_factor=None)
+ 
+        y=y.to("cpu")
+        y=ups(y)
+        residual = torch.abs(y.detach() - upsampled)
         residual = residual.numpy()
 
-        frame = sample["referencePath"][0].split("/")[-1].split(".")[-2]
-        video = sample["referencePath"][0].split("/")[-2]
+        for i in range(y.shape[0]):
+            frame = sample["referencePath"][i].split("/")[-1].split(".")[-2]
+            video = sample["referencePath"][i].split("/")[-2]
 
-        inf_name = f"vid{video}_{frame}_inf.png"
-        res_name = f"vid{video}_{frame}_res.png"
+            inf_name = f"vid{video}_{frame}_inf.png"
+            res_name = f"vid{video}_{frame}_res.png"
+            original_name = f"vid{video}_{frame}_orig.png"
 
-        out = y[0].permute(1,2,0).detach().numpy() * 255
-        residual = residual * 255
-
-        cv2.imwrite(os.path.join(path, inf_name), out)
-        cv2.imwrite(os.path.join(path, res_name), residual)
+            out = y[i,:,:,:].permute(1,2,0).detach().numpy() * 255
+            singleres = residual[i,:,:,:].transpose((1,2,0)) * 255
+            orig = upsampled[i,:,:,:].detach().numpy()
+            orig = orig.transpose((1,2,0)) * 255
+            cv2.imwrite(os.path.join(path, inf_name), out)
+            cv2.imwrite(os.path.join(path, res_name), singleres)
+            cv2.imwrite(os.path.join(path, original_name), orig)
 
         del s 
         del y
+        torch.cuda.empty_cache()
 
         if(testing):
             break
